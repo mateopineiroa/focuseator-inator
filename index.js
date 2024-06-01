b574b84e64c7da3abad031bb08bf46636fbb43b4
Mateo Piñeiro <mateopineiro2001@gmail.com>
2024-06-01 11:10:15 -0300

    Update index.js
    works for netflix and google, refactor youtube

--- index.js
+++ index.js
@@ -9,30 +9,31 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+    // 'use strict'; I want to use window
     console.log('matscript loaded hehe');
-    // Function to focus on the first available search input
-    function focusOnSearchInput() {
-        // Get all input elements on the page
-        const inputElements = document.querySelectorAll('input[type="text"], input[type="search"]');
-        console.log(inputElements);
 
-        // Focus on the first input element if it exists
-        if (inputElements.length > 0) {
-            inputElements[0].focus();
+    function focusOnSearchInput() {
+        // const inputElements = document.querySelectorAll('input[type="text"], input[type="search"]');
+        const inputElements = document.querySelector('input');
+        const textAreaElement = document.querySelector('textarea');
+        const searchButton = document.getElementsByClassName('searchTab');
+
+        if (window.location.origin.includes("netflix")) {
+            return searchButton[0].click()
         }
+        if (inputElements) {
+            return inputElements.focus();
+        }
+        if (textAreaElement) {
+            return textAreaElement.focus();
+        }
+
+
     }
 
-    // Event listener for the key combination (Cmd + K)
     document.addEventListener('keydown', function (event) {
-        console.log('keydown')
-
-        // Check if the key combination is Cmd + K
         if (event.metaKey && event.key === 'k') {
-            // Prevent the default behavior of the key combination
             event.preventDefault();
-
-            // Call the function to focus on the search input
             focusOnSearchInput();
         }
     });
